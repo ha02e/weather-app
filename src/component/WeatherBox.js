419db81c6c2e8c00f9c7eebfd8ab475c26e30a82
hayoung <ha02e@naver.com>
2024-03-24 00:42:21 +0900

    css 수정 - 폰트, 폰트 크기, 색상 등

--- src/component/WeatherBox.js
+++ src/component/WeatherBox.js
@@ -4,13 +4,18 @@ const WeatherBox = ({ weather }) => {
   console.log("weather??", weather);
   return (
     <div className="weather-box">
-      <div>날짜</div>
-      <h2>{weather?.name}</h2>
-      <h1>
-        {weather?.main.temp}
-        &#8451; / {weather?.main.temp * 1.8 + 32}&#8457;
+      {/* <div>날짜</div> */}
+      <h2 className="city-name">{weather?.name}</h2>
+      <h1 className="city-temp">
+        {Math.floor(weather?.main.temp)}
+        &#8451; / {Math.floor(weather?.main.temp * 1.8 + 32)}&#8457;
       </h1>
-      <h3>{weather?.weather[0].description}</h3>
+      <div className="city-weather">
+        <img
+          src={`http://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`}
+        />
+        <h3 className="city-description">{weather?.weather[0].description}</h3>
+      </div>
     </div>
   );
 };
