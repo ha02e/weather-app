7e6395dffd18de9b8240e7fb7e7db359bbc720f4
hayoung <ha02e@naver.com>
2024-03-24 16:10:28 +0900

    오늘 날짜 출력

--- src/component/WeatherBox.js
+++ src/component/WeatherBox.js
@@ -2,14 +2,37 @@ import React from "react";
 
 const WeatherBox = ({ weather }) => {
   console.log("weather??", weather);
+
+  const getTodayDate = () => {
+    let today = new Date();
+
+    let options = {
+      year: "numeric", // 연도 (숫자 형식)
+      month: "2-digit", // 월 (2자리 숫자 형식)
+      day: "2-digit", // 일 (2자리 숫자 형식)
+      hour: "2-digit", // 시간 (2자리 숫자 형식)
+      minute: "2-digit", // 분 (2자리 숫자 형식)
+      second: "2-digit", // 초 (2자리 숫자 형식)
+    };
+
+    return today.toLocaleString("en-US", options).replace(/,/g, "/");
+  };
+  const todayDate = getTodayDate();
+
   return (
     <div className="weather-box">
-      {/* <div>날짜</div> */}
-      <h2 className="city-name">{weather?.name}</h2>
-      <h1 className="city-temp">
-        {Math.floor(weather?.main.temp)}
-        &#8451; / {Math.floor(weather?.main.temp * 1.8 + 32)}&#8457;
-      </h1>
+      <h2 className="city-name">
+        <span class="material-symbols-outlined">location_on</span>
+        <span>{weather?.name}</span>
+      </h2>
+      <div className="today-date">{todayDate}</div>
+      <div>
+        <h1 className="city-temp">
+          {Math.floor(weather?.main.temp)}&#8451;
+          {/*  / {Math.floor(weather?.main.temp * 1.8 + 32)}&#8457; */}
+        </h1>
+        <div>최저기온 / 최고기온</div>
+      </div>
       <div className="city-weather">
         <img
           src={`http://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`}
